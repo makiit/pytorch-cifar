0d7d06666de577d4b83bb526109141dd6630e7af
makiit <mohammadali.khan13@gmail.com>
2022-04-10 01:22:38 -0700

    DSDSD

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=<<test-job>>
+#### Change account below
+#SBATCH --account=ddp390
+#SBATCH --partition=gpu-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=96G
+#SBATCH --gpus=1
+#SBATCH --time=01:00:00
+#SBATCH --output=output.o%j.%N
+
+# load the environments needed
+
+module purge
+module load slurm
+module load gpu
+module load cuda10.2/toolkit/10.2.89
+module list
+
+nvidia-smi
+nvcc -V
+
+
+# run your code
+python main.py
